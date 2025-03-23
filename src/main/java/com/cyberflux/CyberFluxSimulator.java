25bd8736d0ce52a07a1c52bed3250de475fcc9f4
Cassiano <cassianopessoa2003@hotmail.com>
2025-03-22 21:27:36 -0300

    comentarios e ideias no ciberfluxsimulator.java

--- src/main/java/com/cyberflux/CyberFluxSimulator.java
+++ src/main/java/com/cyberflux/CyberFluxSimulator.java
@@ -18,3 +18,13 @@ public class CyberFluxSimulator {
         }
     }
 }
+
+//criar uma solução para evitar deadlock e starvation - algo como limitar o tempo das threads em cada cliente
+//criar um gerenciador de threads para isso (tipo dono da lan house kkk) -> alguma classe que tenha noção e analise a todo tempo os clientes
+//impedir que só um tipo de cliente acesse os recursos
+//criar um tempo de simulaçao fixo (8h que nem diz no trabalho, pode ser 30s de simulação real)
+//ideias para criar as estatisticas finais:
+//                      \_ toda vez que uma thread aloca recurso pra um cliente, incrementa no numero de clientes totais
+//                                                                           \_ criar um atributo contador privado de clientes em recurso (?)
+//                      \_ sistema de espera: não tenho mta ideia do que fazer
+//                      \_ taxa de utilização: criar contador conforme o tempo for passando em cada recurso
